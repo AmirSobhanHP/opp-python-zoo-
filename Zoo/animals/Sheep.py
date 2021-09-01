102979459306f5cc009d58e871a35dadc07d155e
AmirMahdyTurani <81232088+AmirMahdyTurani@users.noreply.github.com>
2021-09-01 19:29:57 +0430

    Update and rename sheep.py to Sheep.py

--- Zoo/animals/Sheep.py
+++ Zoo/animals/Sheep.py
@@ -1,9 +1,19 @@
-class sheep:
+class Sheep:
+    name = ""
+    age = int()
+    color = ""
+    weight = float()
     def __init__(self,name,age,color,weight):
         self.name = name
-        self.age = age
+        if age > 0:
+            self.age = age
+        else:
+            raise ValueError
         self.color = color
-        self.weight = weight
+        if weight > 0.0:
+            self.weight = weight
+        else:
+            raise ValueError
     def eating(self,nameOfFood):
         return f"{self.name} eating {nameOfFood}"
     def sheepinfo(self):
